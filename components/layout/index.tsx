d3394fcda583674b37340bebff06f7c72054802f
joshua <iuttn123@gmail.com>
2022-05-24 18:58:07 +0900

    start

---
+++ components/layout/index.tsx
@@ -0,0 +1,18 @@
+import {FC} from "react";
+import Head from "next/head";
+
+
+const Layout: FC = () => {
+    return (
+        <>
+            <Head>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width, minimal-ui"/>
+                <title>웰컴</title>
+            </Head>
+            
+
+        </>
+    );
+}
+
+export default Layout;
